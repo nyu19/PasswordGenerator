871036b1dc58a5327fa64db17f6d8fe603e03951
Nakul Upasani <69073624+nyu19-py@users.noreply.github.com>
2021-06-07 14:53:11 +0530

    Init Commit.

---
+++ app/src/main/java/me/nyu19/passwordgenerator/MainActivity.java
@@ -0,0 +1,54 @@
+package me.nyu19.passwordgenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private TextView textView;
+    private EditText outputField;
+
+    public void generatePassword(View v){
+        Switch upper = findViewById(R.id.upper);
+        Switch lower = findViewById(R.id.lower);
+        Switch nums = findViewById(R.id.numeric);
+        Switch spChar = findViewById(R.id.spchar);
+        try{
+            outputField.setText(new PasswordGenerator3000().makePassword(seekBar.getProgress(),
+                    upper.isChecked(),
+                    lower.isChecked(),
+                    nums.isChecked(),
+                    spChar.isChecked()
+            ));
+        }catch (NullPointerException nl){
+            Toast.makeText(this,"Please select atleast 1 option",Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        textView = (TextView) findViewById(R.id.textViewSeekBar);
+        outputField = findViewById(R.id.outputField);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(progress+"");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+}
